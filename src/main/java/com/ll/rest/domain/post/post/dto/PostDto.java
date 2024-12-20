c04f1059b81ea74e67404434633ee24a197e3753
Gyeonghun Min <riverpower6@naver.com>
2024-12-20 14:33:33 +0900

    up to 57

--- src/main/java/com/ll/rest/domain/post/post/dto/PostDto.java
+++ src/main/java/com/ll/rest/domain/post/post/dto/PostDto.java
@@ -1,6 +1,7 @@
 package com.ll.rest.domain.post.post.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.ll.rest.domain.member.member.dto.MemberDto;
 import com.ll.rest.domain.post.post.entity.Post;
 import java.time.LocalDateTime;
 import lombok.Getter;
@@ -12,6 +13,7 @@ public class PostDto {
     private LocalDateTime createDate;
     @JsonProperty("modifiedDateTime")
     private LocalDateTime modifyDate;
+    private MemberDto author;
     private String title;
     private String content;
 
@@ -19,6 +21,7 @@ public class PostDto {
         this.id = post.getId();
         this.createDate = post.getCreateDate();
         this.modifyDate = post.getModifyDate();
+        this.author = new MemberDto(post.getAuthor());
         this.title = post.getTitle();
         this.content = post.getContent();
     }
