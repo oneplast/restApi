0f808d32b739aa246d1d458b71acdf4d66a46381
Gyeonghun Min <riverpower6@naver.com>
2024-12-20 12:40:04 +0900

    up to 48

---
+++ src/main/java/com/ll/rest/domain/member/member/controller/MemberController.java
@@ -0,0 +1,4 @@
+package com.ll.rest.domain.member.member.controller;
+
+public class MemberController {
+}
---
+++ src/main/java/com/ll/rest/domain/member/member/entity/Member.java
@@ -0,0 +1,27 @@
+package com.ll.rest.domain.member.member.entity;
+
+import com.ll.rest.global.jpa.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Member extends BaseEntity {
+    @Column(unique = true, length = 30)
+    private String username;
+
+    @Column(length = 50)
+    private String password;
+
+    @Column(length = 30)
+    private String nickname;
+}
