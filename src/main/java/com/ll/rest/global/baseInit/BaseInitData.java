4c8a31d393c2bf3e10895f88f97f5d6855a87510
Gyeonghun Min <riverpower6@naver.com>
2024-12-23 10:41:38 +0900

    up to 61

--- src/main/java/com/ll/rest/global/baseInit/BaseInitData.java
+++ src/main/java/com/ll/rest/global/baseInit/BaseInitData.java
@@ -40,6 +40,8 @@ public class BaseInitData {
         memberService.join("user1", "1234", "유저1");
         memberService.join("user2", "1234", "유저2");
         memberService.join("user3", "1234", "유저3");
+        memberService.join("user4", "1234", "유저4");
+        memberService.join("user5", "1234", "유저5");
     }
 
     @Transactional
@@ -50,9 +52,13 @@ public class BaseInitData {
 
         Member memberUser1 = memberService.findByUsername("user1").get();
         Member memberUser2 = memberService.findByUsername("user2").get();
+        Member memberUser3 = memberService.findByUsername("user3").get();
+        Member memberUser4 = memberService.findByUsername("user4").get();
 
         postService.write(memberUser1, "축구 하실 분?", "14시까지 22명을 모아야 합니다.");
         postService.write(memberUser1, "배구 하실 분?", "15시까지 12명을 모아야 합니다.");
         postService.write(memberUser2, "농구 하실 분?", "16시까지 10명을 모아야 합니다.");
+        postService.write(memberUser3, "발야구 하실 분?", "17시까지 14명을 모아야 합니다.");
+        postService.write(memberUser4, "피구 하실 분?", "18시까지 18명을 모아야 합니다.");
     }
 }
